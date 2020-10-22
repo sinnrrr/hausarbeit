0aace715935ff0df7d5991ea40b3a52e938601ab
sinnrrr <dimasoltusyuk@gmail.com>
2020-10-22 23:46:31 +0300

    fuck this shit

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,6 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    '@nuxt/content',
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
