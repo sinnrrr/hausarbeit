68c6ef821c08a4826476c53107a3bf7cb33812f1
sinnrrr <dimasoltusyuk@gmail.com>
2020-10-22 23:11:11 +0300

    fuck this shit

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <section class="landing">
     <lazy-blockquote />
     <div class="landing__buttons">
-      <nuxt-link class="landing__button" to="/facts/1">Take a tour</nuxt-link>
+      <nuxt-link class="landing__button" to="/facts/1">Факти</nuxt-link>
       <a
         class="landing__button"
         href="https://calendar.google.com/calendar/u/0?cid=NXM2ODFyYzIwa2kxYjMyc2JmdGo4ZmJpNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
       >
-        Calendar
+        Нагадування
       </a>
     </div>
   </section>
--- pages/menu.vue
+++ pages/menu.vue
@@ -2,21 +2,24 @@
   <section class="menu">
     <div class="menu_wrapper">
       <div class="menu__container">
-        <nuxt-link to="/hello">Hello</nuxt-link>
-        <nuxt-link to="/from">from</nuxt-link>
-        <nuxt-link to="/menu">menu</nuxt-link>
-        <nuxt-link to="/component">component</nuxt-link>
-      </div>
-      <div class="menu__container">
-        <h1>Menu</h1>
-        <div>close</div>
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at
-          beatae consequuntur cumque debitis, eaque expedita fuga impedit
-          incidunt magnam, odit officia officiis porro quae quam sapiente
-          suscipit totam vero!</span
+        <nuxt-link to="/">Головна</nuxt-link>
+        <nuxt-link to="/from">Факти</nuxt-link>
+        <a
+          href="https://calendar.google.com/calendar/u/0?cid=NXM2ODFyYzIwa2kxYjMyc2JmdGo4ZmJpNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+          >Нагадування</a
         >
+        <nuxt-link to="/component">Зворотній зв'язок</nuxt-link>
       </div>
+      <!--      <div class="menu__container">-->
+      <!--        <h1>Menu</h1>-->
+      <!--        <div>close</div>-->
+      <!--        <span>-->
+      <!--          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at-->
+      <!--          beatae consequuntur cumque debitis, eaque expedita fuga impedit-->
+      <!--          incidunt magnam, odit officia officiis porro quae quam sapiente-->
+      <!--          suscipit totam vero!</span-->
+      <!--        >-->
+      <!--      </div>-->
     </div>
   </section>
 </template>
