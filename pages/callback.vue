28b6e1dec03667cd974197ce87199694b449c518
sinnrrr <dimasoltusyuk@gmail.com>
2020-10-24 00:10:22 +0300

    test

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,12 +1,11 @@
 <template>
-  <section class="callback">
+  <formulate-form class="callback">
     <h1>Зв'язатись з розробником</h1>
     <p>
       Якщо ви маєте ідеї щодо покращення вигляду/роботи сайту, чи зауваження
       можете залишити повідомлення використовучючи дану форму.
     </p>
-    <button @click="sendMail('a', 'a', 'a')"></button>
-  </section>
+  </formulate-form>
 </template>
 
 <script>
